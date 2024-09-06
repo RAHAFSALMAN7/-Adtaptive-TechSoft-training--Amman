2efc79b03bafe355b671765ee503a0a6433e929e
Rahafsalman <144127007+RAHAFSALMAN7@users.noreply.github.com>
2024-09-06 14:55:45 +0300

    Add files via upload

---
+++ src/client/index.js
@@ -0,0 +1,17 @@
+ import "./public/style/style.scss";
+
+ import { handleSubmit } from "./script/handleForm";
+
+ if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then((registration) => {
+      console.log('Service Worker registered with scope:', registration.scope);
+    }).catch((error) => {
+      console.error('Service Worker registration failed:', error);
+    });
+  });
+}
+
+ export {
+  handleSubmit
+};
